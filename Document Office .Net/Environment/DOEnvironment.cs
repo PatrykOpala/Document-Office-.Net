db7ce374bc58863ebe85ef6b89c754954f13a8ea
Patryk Opala <patrykopala265@gmail.com>
2024-06-11 13:30:59 +0200

    0.0.44 debug 3

--- Document Office .Net/Environment/DOEnvironment.cs
+++ Document Office .Net/Environment/DOEnvironment.cs
@@ -5,6 +5,21 @@ using System.Windows.Forms;
 
 namespace Document_Office.Net.Environment
 {
+
+    internal class LabelTag
+    {
+        public Guid _guid;
+        public String _oldValue;
+        public String _newValue;
+
+        public LabelTag(Guid guid, String oldValue, String newValue = "")
+        {
+            this._guid = guid;
+            this._oldValue = oldValue;
+            this._newValue = newValue;
+        }
+    }
+
    internal class DOEnvironmentUI
     {
         private List<IDOElement> elements = new List<IDOElement>();
@@ -21,7 +36,12 @@ namespace Document_Office.Net.Environment
 
         public void SaveToFile()
         {
+            foreach (Control element in _newspaperPanel.Controls)
+            {
+                var text = element.Text;
+                var textArray = text.Split();
 
+            }
         }
 
         private void PrepareEnvContainer(int width, int height, int x, int y, Form form, string TestText) 
@@ -42,7 +62,6 @@ namespace Document_Office.Net.Environment
                 TextAlign = ContentAlignment.MiddleCenter,
                 Font = new Font("Arial", 30.0f),
                 Size = new Size(30, 50),
-                BackColor= Color.Teal,
             };
             EnvPanel.Controls.Add(textBox);
 
@@ -60,25 +79,11 @@ namespace Document_Office.Net.Environment
                 Location = new Point(containerWidth / 2 - 460, 50),
                 AutoSize = true,
             };
-
-            /*if(this.elements.Count > 0 )
-            {
-                foreach( IDOElement idoElement in this.elements )
-                {
-                    Console.WriteLine(idoElement.Type);
-
-                    if(idoElement.Type == "Paragraph")
-                    {
-                        newspaperPanel.Controls.Add(CreateNewspaperParagraph(new DOParagraph()));
-                    }
-                }
-            }*/
-
             _newspaperPanel = newspaperPanel;
             return newspaperPanel;
         }
 
-        public void CreateNewspaperParagraph(DOParagraph paragraph, ref int vertical)
+       /* public void CreateNewspaperParagraph(DOParagraph paragraph, ref int vertical)
         {
             foreach(DORun dORun in paragraph.ListRuns)
             {
@@ -88,34 +93,144 @@ namespace Document_Office.Net.Environment
                     BorderStyle = BorderStyle.FixedSingle,
                     Cursor = Cursors.Hand,
                     AutoSize = true,
-                    Tag = dORun.DORunGuid,
+                    Tag = new LabelTag(dORun.DORunGuid, dORun.Text),
                     Font = dORun.Properties.Font,
                     ForeColor = Color.Black,
                     Text = dORun.Text
-                    //dORun.Properties._Color
                 };
-
-                if (dORun.Text == "")
-                    label.Text = "[Pusty]";
 
                 Graphics gfx = Graphics.FromImage(new Bitmap(1, 1));
                 SizeF stringSize = gfx.MeasureString(dORun.Text, dORun.Properties.Font);
 
                 label.Text = dORun.Text;
 
+                if (dORun.Text == "")
+                {
+                    label.Text = "[Pusty]";
+                }
+
                 if (dORun.Text == " ")
                 {
                     label.Location = new Point(vertical + ((int)stringSize.Width) - 17, _y);
-                    //vertical += label.Size.Width;
-                    label.Text = " ";
+                    label.Text = dORun.Text;
                 }
 
+                label.Click += Label_Click;
+
                 _newspaperPanel.Controls.Add(label);
-                Console.WriteLine(stringSize.Width);
                 vertical += label.Size.Width + 5;
-                //((int)stringSize.Width)
             }
             _y += 100;
+        }*/
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            Label targetLabel = ((Label)sender);
+
+            foreach (var text in targetLabel.Text.Split(' '))
+            {
+                var j = text.ToCharArray();
+                foreach (var i in j)
+                {
+                    Console.WriteLine($"{((short)i)}");
+                }
+            }
+
+            //Console.WriteLine($"{((LabelTag)targetLabel.Tag)._oldValue}");
+            /*foreach(var text in targetLabel.Text.Split(' '))
+            {
+                var j = text.ToCharArray();
+                foreach(var i in j)
+                {
+                    Console.WriteLine($"{((short)i)}");
+                }
+            }*/
+
+            /*Panel Click_Panel = new Panel()
+            {
+                Size = new Size(400, 200),
+                Location = new Point(targetLabel.Bounds.X, targetLabel.Bounds.Y + 20),
+                BackColor = Color.Brown,
+            };
+
+            Label Old_Value_Label = new Label()
+            {
+                Text = "Poprzednia wartość",
+                Location = new Point(5,20),
+                ForeColor = Color.White,
+                Font = new Font("Arial", 15),
+                AutoSize = true
+            };
+
+            TextBox Old_Value_TextBox = new TextBox()
+            {
+                Location = new Point(Old_Value_Label.Bounds.X, Old_Value_Label.Bounds.Y + 30),
+                Size = new Size(160, 50),
+                Text = targetLabel.Text,
+                Font = new Font("Arial", 15),
+            };
+
+            Label New_Value_Label = new Label()
+            {
+                Text = "Nowa wartość",
+                Location = new Point(Old_Value_Label.Size.Width + 110, Old_Value_Label.Location.Y),
+                ForeColor = Color.White,
+                Font = new Font("Arial", 15),
+                AutoSize = true
+            };
+
+            TextBox New_Value_TextBox = new TextBox()
+            {
+                Location = new Point(New_Value_Label.Bounds.X, New_Value_Label.Bounds.Y + 30),
+                Size = new Size(140, 30),
+                Font = new Font("Arial", 15),
+            };
+
+            targetLabel.Parent.Controls.Add(Click_Panel);
+            targetLabel.Parent.Controls.SetChildIndex(Click_Panel, 0);
+
+            Click_Panel.Controls.Add(Old_Value_Label);
+            Click_Panel.Controls.Add(Old_Value_TextBox);
+
+            Click_Panel.Controls.Add(New_Value_Label);
+            Click_Panel.Controls.Add(New_Value_TextBox);*/
+
+        }
+
+        public void CreateNewspaperParagraph(DOParagraph paragraph, ref int vertical)
+        {
+            string paragraphText = "";
+            foreach (DORun dORun in paragraph.ListRuns)
+            {
+                paragraphText += dORun.Text;
+            }
+            Graphics gfx = Graphics.FromImage(new Bitmap(1, 1));
+            SizeF stringSize;
+            Font font;
+            if (paragraph.ListRuns.Length > 1)
+            {
+                font = paragraph.ListRuns[1].Properties.Font;
+                stringSize = gfx.MeasureString(paragraphText, paragraph.ListRuns[1].Properties.Font);
+            }
+            else
+            {
+                font = paragraph.ListRuns[0].Properties.Font;
+                stringSize = gfx.MeasureString("[Pusty Akapit]", paragraph.ListRuns[0].Properties.Font);
+            }
+
+            TextBox textBox = new TextBox()
+            {
+                Location = new Point(vertical, _y),
+                BorderStyle = BorderStyle.FixedSingle,
+                Size = new Size((int)stringSize.Width, (int)stringSize.Height),
+                Font = font,
+                ForeColor = Color.Black,
+                Text = paragraphText.Length < 1 ? "[Pusty Akapit]" : paragraphText,
+            };
+
+            _newspaperPanel.Controls.Add(textBox);
+            _y += 90;
+            paragraphText = "";
         }
 
         public void CreateNewspaperTable(DOTable Table)
@@ -162,30 +277,6 @@ namespace Document_Office.Net.Environment
         public void InitUI(int width)
         {
             List<IDOElement> dOElements = new List<IDOElement>();
-            /*foreach(Guid elementGuid in ido)
-            //{
-            //    foreach(DOParagraph paragraph in DocsParagraphElements)
-            //    {
-            //        if(paragraph != null)
-            //        {
-            //            if(paragraph.IDOElementGuid == elementGuid)
-            //            {
-            //                dOElements.Add(paragraph);
-            //            }
-            //        }
-            //    }
-
-            //    foreach(DOTable table in DocsTableElements)
-            //    {
-            //        if (table != null)
-            //        {
-            //            if (table.IDOElementGuid == elementGuid)
-            //            {
-            //                dOElements.Add(table);
-            //            }
-            //        }
-            //    }
-            }*/
 
             int xEnv = 0;
             int yEnv = 0;
@@ -194,7 +285,6 @@ namespace Document_Office.Net.Environment
             {
                 DOEnvironmentUI envUI = new DOEnvironmentUI();
                 envUI.AddElementsToList(dOElements);
-                Console.WriteLine($"CountCopies: {countCopies}");
                 if (countCopies > 2)
                     envUI.GenerateUI(width + 135, 600, xEnv, yEnv, EnvironmentFileName, RootWindow, iterator.ToString());
                 else
